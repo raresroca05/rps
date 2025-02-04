433005ff53c4849fcbf3d6e9a984c333f5280815
Rares Roca <rares.roca05@gmail.com>
2025-02-04 01:12:00 +0000

    Add games controller with play action

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class GamesController < ApplicationController
+  def index
+    @throws = Game::Rules.throws
+  end
+
+  def play
+    player_throw = validate_throw_param!
+    opponent_result = Api::ThrowClient.fetch
+
+    @result = Game::Resolver.new(
+      player_throw: player_throw,
+      opponent_throw: opponent_result.throw_name
+    )
+    @used_fallback = opponent_result.fallback?
+
+    respond_to do |format|
+      format.html { render :result }
+      format.turbo_stream { render :result }
+    end
+  end
+
+  private
+
+  def validate_throw_param!
+    throw_param = params[:throw]&.downcase
+
+    unless throw_param.present? && Game::Rules.valid_throw?(throw_param)
+      redirect_to root_path, alert: "Invalid throw selection"
+      return nil
+    end
+
+    throw_param.to_sym
+  end
+end
